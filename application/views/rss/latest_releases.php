045940a8b641b35e196d7cd3f570504b7d9b298a
Cj Malone <Cj-Malone@users.noreply.github.com>
2017-04-22 09:38:41 +0100

    Fix/improve RSS feeds
    This squashes 4 smaller commits from Cj Malone and adds some more
    fixes to make RSS feeds pass the W3C validator
    (https://validator.w3.org/feed).
    
    Change-Id: Ibe786b44d60aa7f8b953fa7e3d5c592998a51ba1

--- application/views/rss/latest_releases.php
+++ application/views/rss/latest_releases.php
@@ -1,38 +1,22 @@
 <?php header("Content-Type: application/rss+xml; charset=utf-8"); ?>
 <?php echo '<?xml version="1.0" ?>' ?>
-<rss version="2.0">
-
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
-
-<title>LibriVox's New Releases</title>
-
-<link>https://librivox.org</link>
-
-<description>LibriVox volunteers record chapters of books in the public domain and release the audio files back onto the net. Our goal is to make all public domain books available as free audio books. We are a totally volunteer, open source, free content, public domain project.</description>
-
-
-<!-- project loop -->
-
-<?php foreach($projects as $project): ?>
-
+  <title>LibriVox's New Releases</title>
+  <link>http://librivox.org</link>
+  <atom:link href="https://librivox.org/rss/latest_releases" rel="self" type="application/rss+xml" />
+  <description>LibriVox volunteers record chapters of books in the public domain and release the audio files back onto the net. Our goal is to make all public domain books available as free audio books. We are a totally volunteer, open source, free content, public domain project.</description>
+  <!-- project loop -->
+  <?php foreach($projects as $project): ?>
 	<?php $date_catalog = DateTime::createFromFormat('Y-m-d', $project->date_catalog); 	?>
-
-<item>
-
-<title><![CDATA[<?= $project->title_bar?>]]></title>
-<description><![CDATA[<?= $project->description?>]]></description>
-
-<link><![CDATA[<?= $project->url_librivox?>]]></link>
-<guid><![CDATA[<?= $project->url_librivox?>]]></guid>
-
-<pubDate><![CDATA[<?= $date_catalog->format('D, d M Y'); ?>]]></pubDate>
-
-</item>
-
-<?php endforeach; ?>
-
-<!-- end file loop -->
-
+  <item>
+    <title><![CDATA[<?= $project->title_bar?>]]></title>
+    <description><![CDATA[<?= $project->description?>]]></description>
+    <link><![CDATA[<?= $project->url_librivox?>]]></link>
+    <guid><![CDATA[<?= $project->url_librivox?>]]></guid>
+    <pubDate><![CDATA[<?= $date_catalog->format('r'); ?>]]></pubDate>
+  </item>
+  <?php endforeach; ?>
+  <!-- end file loop -->
 </channel>
-
 </rss>
--- application/views/rss/rss.php
+++ application/views/rss/rss.php
@@ -1,53 +1,45 @@
-<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/" xmlns:creativeCommons="http://blogs.law.harvard.edu/tech/creativeCommonsRssModule" version="2.0">
-
+<?php header("Content-Type: application/rss+xml; charset=utf-8"); ?>
+<?php echo '<?xml version="1.0" ?>' ?>
+<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/" xmlns:creativeCommons="http://backend.userland.com/creativeCommonsRssModule" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 <channel>
+  <title><![CDATA[<?= $project->title_bar ?>]]></title>
+  <link><![CDATA[<?= $project->url_librivox ?>]]></link>
+  <atom:link rel="self" href="https://librivox.org/rss/<?= $project->id ?>" />
+  <description><![CDATA[<?= strip_tags($project->description) ?>]]></description>
+  <!--<genre>project element=Genre</genre>-->
+  <!--<language>project element=lang.code</language>-->
+  <itunes:author>LibriVox</itunes:author>
+  <itunes:summary><![CDATA[<?= strip_tags($project->description) ?>]]></itunes:summary>
+  <itunes:owner>
+    <itunes:name>LibriVox</itunes:name>
+    <itunes:email>info@librivox.org</itunes:email>
+  </itunes:owner>
+  <itunes:category text="Arts">
+    <itunes:category text="Literature" />
+  </itunes:category>
+  <!-- file loop -->
+  <?php foreach($sections as $section): ?>
+  <item>
+    <title><![CDATA[<?= $section->title?>]]></title>
+    <!--<reader>file element=reader</reader> -->
+    <link><![CDATA[<?=  $project->url_librivox?>]]></link>
+      <?php /* 1 kbps is equivalent to 125 bytes per second. Our 64kbps
+               bitrate is therefore equivalent to 8000 bytes per second. We
+               could therefore use playtime * 8000 to calculate the enclosure
+               length in bytes as the standard demands. However, that doesn't
+               give us the exact file size in bytes. In case of unknown
+               enclosure length, the W3C recommends we use 0 [1], so that's
+               what we do.
 
-<title><![CDATA[<?= $project->title_bar ?>]]></title>
-
-<link><![CDATA[<?= $project->url_librivox ?>]]></link>
-
-<description><![CDATA[<?= $project->description ?>]]></description>
-
-<genre><!-- project element=Genre --></genre>
-
-<language><!-- project element=lang.code --></language>
-
-<itunes:author>LibriVox</itunes:author>
-
-<itunes:summary><![CDATA[<?= $project->description ?>]]></itunes:summary>
-
-
-<itunes:owner>
-  <itunes:name>LibriVox</itunes:name>
-  <itunes:email>info@librivox.org</itunes:email>
-</itunes:owner>
-
-
-<itunes:category text="Arts"><itunes:category text="Literature" /></itunes:category>
-
-
-<!-- file loop -->
-
-
-<?php foreach($sections as $section): ?>
-
-<item>
-
-<title><![CDATA[<?= $section->title?>]]></title>
-<reader><!-- file element=reader --></reader>
-<link><![CDATA[<?= $section->mp3_64_url?>]]></link>
-<enclosure url="<?= $section->mp3_64_url?>" length="<?= $section->mp3_64_size?>" type="audio/mpeg" />
-
-<itunes:explicit>No</itunes:explicit>
-<itunes:block>No</itunes:block>
-<itunes:duration><![CDATA[<?= $section->migrated_time?>]]></itunes:duration>
-<pubDate><!-- file element=rss.pubDate --></pubDate>
-<media:content url="<?= $section->mp3_64_url?>" fileSize="<?= $section->mp3_64_size?>" type="audio/mpeg" />
-</item>
-
-<?php endforeach; ?>
-<!-- end file loop -->
-
+               [1] https://validator.w3.org/feed/docs/error/UseZeroForUnknown.html */ ?>
+      <enclosure url="<?= $section->mp3_64_url?>" length="0" type="audio/mpeg" />
+  <itunes:explicit>No</itunes:explicit>
+  <itunes:block>No</itunes:block>
+  <itunes:duration><![CDATA[<?= $section->migrated_time?>]]></itunes:duration>
+  <!--<pubDate>file element=rss.pubDate</pubDate>-->
+  <media:content url="<?= $section->mp3_64_url?>" type="audio/mpeg" />
+ </item>
+  <?php endforeach; ?>
+  <!-- end file loop -->
 </channel>
-
 </rss>
