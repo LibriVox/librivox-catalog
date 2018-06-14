58b369bd5bde6e98f405d313ab7586321b88ea38
Roland Illig <roland.illig@gmx.de>
2018-06-15 00:00:01 +0200

    Display author information correctly
    If an author has 0 completed books, that information is not worth being
    displayed. Same for 0 books in progress. Use correct plural forms for
    the above information (book/books).
    
    When updating the book statistics, also update those authors that don't
    have any books at all. Without this extra cleanup, these authors will
    keep the previous count of their books, which is wrong. To see the wrong
    behavior:
    
        UPDATE authors SET meta_complete = -1, meta_in_progress = -1;
    
        https://librivox.org/cron/project_status_stats/author
    
        https://librivox.org/search
    
    Now some of the authors still have the -1 count.
    
    Change-Id: Idcfbff544774c86d2dc1502aa0f7e344daabd5c2

--- application/views/catalog/item_blades/author_blade.php
+++ application/views/catalog/item_blades/author_blade.php
@@ -6,7 +6,19 @@
 
 	<div class="result-data">
 		<h3><a href="<?= base_url('author/'. $item['author_id']) ?>"><?= build_author_name((object) $item)?> <span class="dod-dob"><?= build_author_years((object) $item) ?></span></a></h3>
-		<p class="book-meta">Completed: <span><?= $item['meta_complete']?> books</span> | In progress: <span><?= $item['meta_in_progress']?> books</span></p>
+		<p class="book-meta">
+			<?php if ($item['meta_complete'] > 1): ?>
+				Completed: <span><?= $item['meta_complete']?> books</span>
+			<?php elseif ($item['meta_complete'] == 1): ?>
+				Completed: <span><?= $item['meta_complete']?> book</span>
+			<?php endif; ?>
+			<?= $item['meta_complete'] > 0 && $item['meta_in_progress'] > 0 ? '|' : '' ?>
+			<?php if ($item['meta_in_progress'] > 1): ?>
+				In progress: <span><?= $item['meta_in_progress']?> books</span>
+			<?php elseif ($item['meta_in_progress'] == 1): ?>
+				In progress: <span><?= $item['meta_in_progress']?> book</span>
+			<?php endif; ?>
+		</p>
 	</div>	
 	<a href="<?= base_url('author/'. $item['author_id']) ?>" class="more-result">more results</a>		
 </li>
