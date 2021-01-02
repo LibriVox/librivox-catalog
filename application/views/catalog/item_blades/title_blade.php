6d435553bf87622c64361fcdcac9c39b3e52a683
Kevin Groeneveld <kgroeneveld@gmail.com>
2021-01-01 21:01:42 -0500

    Fix image link href in browse catalog title blade.
    The image link href was hard coded to "#". Now it links to the
    corresponding title.
    
    The Title URL was already being built up in two different spots. This is
    now done once and stored as $url and used for the image and the other
    spots. The image source logic was also refactored into the same set of
    if statements to make the code clearer.

--- application/views/catalog/item_blades/title_blade.php
+++ application/views/catalog/item_blades/title_blade.php
@@ -4,11 +4,24 @@
 		<div class="catalog-type"><span class="title-icon"></span>Title</div>
 	<?php endif; ?>
 
+	<?php
+		if ($item['status'] == PROJECT_STATUS_COMPLETE)
+		{
+			if (empty($item['coverart_thumbnail']))
+				$image = base_url() . 'images/book-cover-default-65x65.gif';
+			else
+				$image = $item['coverart_thumbnail'];
 
-	<?php $image = (empty($item['coverart_thumbnail']))? base_url().'images/book-cover-default-65x65.gif': $item['coverart_thumbnail'] ;?>
-	<?php $image = ($item['status'] == 'complete') ? $image  : base_url().'images/book-cover-in-progress-65x65.gif' ?>			
+			$url = $item['url_librivox'];
+		}
+		else
+		{
+			$image = base_url() . 'images/book-cover-in-progress-65x65.gif';
+			$url = $item['url_forum'];
+		}
+	?>
 
-	<a href="#" class="book-cover"><img src="<?= $image ?>" alt="book-cover-65x65" width="65" height="65" /></a>	
+	<a href="<?= $url ?>" class="book-cover"><img src="<?= $image ?>" alt="book-cover-65x65" width="65" height="65" /></a>
 
 	<?php 
 		if (!empty($item['author_list']))
@@ -25,9 +38,9 @@
 
 	<div class="result-data">
 		<?php if ( isset($item['source_table']) &&  $item['source_table'] == 'sections'): ?>
-			<h3><?= $item['search_field']?> (in <a href="<?= ($item['status'] == PROJECT_STATUS_COMPLETE) ? $item['url_librivox'] : $item['url_forum'] ?>"> <?= create_full_title($item)?></a>)</h3>
+			<h3><?= $item['search_field']?> (in <a href="<?= $url ?>"> <?= create_full_title($item)?></a>)</h3>
 		<?php else: ?>
-			<h3><a href="<?= ($item['status'] == PROJECT_STATUS_COMPLETE) ? $item['url_librivox'] : $item['url_forum'] ?>"><?= create_full_title($item)?></a></h3>
+			<h3><a href="<?= $url ?>"><?= create_full_title($item)?></a></h3>
 		<?php endif; ?>
 
 		<p class="book-author"> <?= $authors ?> </p>
