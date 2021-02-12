902439e97c6b5ae1b4cd37c8f708921005156fb9
Kevin Groeneveld <kgroeneveld@gmail.com>
2021-02-11 20:03:16 -0500

    Add Portuguese translations for uploader page.

--- application/language/portuguese/project_launch_uploader_lang.php
+++ application/language/portuguese/project_launch_uploader_lang.php
@@ -1,15 +1,15 @@
 <?php
-$lang['proj_launch_uploader_intro'] = 'When you have finished recording and editing, please use this page to upload your files. You can only upload .mp3, .wav, .flac, .zip and .txt files. The size limit is 100MB. The temporary URLs given out by this uploader are for internal LibriVox use ONLY. Please do NOT post them to any site other than the LibriVox forum and do not share them with non-LibriVox people.';
-$lang['proj_launch_uploader_instructions'] = 'Instructions';
-$lang['proj_launch_uploader_instruct_1'] = 'Use the "Add files" button to select the file(s) from your computer. You can upload multiple files at once, as long as they are all for the same project.';
-$lang['proj_launch_uploader_instruct_2'] = 'Choose the MC for your project from the dropdown list.';
-$lang['proj_launch_uploader_instruct_3'] = 'You can use the blue "Start upload" button to upload all files at once, or else upload each file individually using its own button.';
-$lang['proj_launch_uploader_instruct_4'] = 'Once you have finished uploading all your files, please copy each URL and paste it into a post in the project thread.';
-$lang['proj_launch_uploader_notes'] = 'Notes';
-$lang['proj_launch_uploader_notes_1'] = 'If you don\'t post the URLs in the forum, no one will know the files have been sent!';
-$lang['proj_launch_uploader_notes_2'] = 'If you\'re sending a test file, follow your BC\'s instructions or the instructions here: <a href="https://wiki.librivox.org/index.php/1-Minute_Test">https://wiki.librivox.org/index.php/1-Minute_Test</a>';
-$lang['proj_launch_uploader_notes_3'] = 'If you accidentally select incorrect files, before uploading you can cancel some or all files with the \'Cancel\' button.';
-$lang['proj_launch_uploader_notes_4'] = 'If you upload a file twice (with the same filename), the first upload will be renamed so it is not overwritten.';
-$lang['proj_launch_uploader_notes_5'] = 'Clicking \'Clear\' removes the file from the list, but does not delete it from the server. To have files deleted, contact an admin.';
-$lang['proj_launch_uploader_help'] = 'Having a problem with the uploader? Please try again, with a different browser if you can. If it still doesn\'t work, contact an admin in the forum.';
+$lang['proj_launch_uploader_intro'] = 'Quando você tiver terminado de gravar e editar, por favor use esta página para enviar seus arquivos. Você só pode enviar arquivos dos tipos .mp3, .wav, .flac, .zip e .txt. O limite de tamanho é 100MB. As URL temporárias geradas por este uploader são APENAS para uso interno do LibriVox. Por favor, NÃO poste-as em outros sites que não o fórum do LibriVox e não as compartilhe com pessoas fora do LibriVox.';
+$lang['proj_launch_uploader_instructions'] = 'Instruções';
+$lang['proj_launch_uploader_instruct_1'] = 'Use o botão "Adicionar arquivos" para selecionar o(s) arquivo(s) do seu computador. Você pode enviar vários arquivos ao mesmo tempo, contanto que sejam todos para o mesmo projeto.';
+$lang['proj_launch_uploader_instruct_2'] = 'Escolha o MC do seu projeto da lista.';
+$lang['proj_launch_uploader_instruct_3'] = 'Você pode usar o botão azul "Começar o envio" para enviar todos os arquivos ao mesmo tempo, ou enviar cada arquivo separadamente usando o botão próprio de cada um.';
+$lang['proj_launch_uploader_instruct_4'] = 'Quando você tiver terminado de enviar todos os seus arquivos, por favor copie cada URL e cole-a em uma mensagem no tópico do projeto.';
+$lang['proj_launch_uploader_notes'] = 'Observações:';
+$lang['proj_launch_uploader_notes_1'] = 'Se você não postar o link no fórum, ninguém saberá que você enviou arquivos!';
+$lang['proj_launch_uploader_notes_2'] = 'Se você está enviando um teste, siga as instruções de seu coordenador, ou as instruções aqui:<a href="https://wiki.librivox.org/index.php?title=Teste_de_1_minuto">https://wiki.librivox.org/index.php?title=Teste_de_1_minuto</a>';
+$lang['proj_launch_uploader_notes_3'] = 'Se você acidentalmente selecionar arquivos errados, antes de enviar você pode cancelar alguns ou todos os arquivos com o botão de "Cancelar".';
+$lang['proj_launch_uploader_notes_4'] = 'Se você enviar um mesmo arquivo duas vezes (com o mesmo nome), o primeiro envio será renomeado, logo, ele não é apagado.';
+$lang['proj_launch_uploader_notes_5'] = 'Clicar em "Limpar" remove o arquivo da lista, mas não o apaga do servidor. Para apagar arquivos, contate um administrador.';
+$lang['proj_launch_uploader_help'] = 'Tendo problemas com o uploader? Por favor, tente novamente, com um navegador diferente, se você puder. Se ainda assim não funcionar, contate um administrador no fórum.';
 ?>
