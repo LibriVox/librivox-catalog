657d609feb374aff96daa17f2a546bcf2465bfb5
Kevin Groeneveld <kgroeneveld@gmail.com>
2021-02-11 20:02:33 -0500

    Add Simplified Chinese language files.

---
+++ application/language/zh_cn/project_launch_uploader_lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['proj_launch_uploader_intro'] = '请用这个网页上载已经录制、剪辑完了的音频。允许格式仅限 .mp3，.wav，.flac，.zip，以及 .txt。文件大小限制为100MB。这个上载网页生成的临时链接仅供 LibriVox 内部使用。请勿在任何 LibriVox 论坛以外的网站上发布这些临时链接，或是分享给 LibriVox 组织外的人。';
+$lang['proj_launch_uploader_instructions'] = '请按以下步骤操作：';
+$lang['proj_launch_uploader_instruct_1'] = '点击"Add files"（添加文件）按钮，选择要从你的电脑端上载的文件（可以多选）。你可以一次上载多个音频，只要它们都属于同一个项目。';
+$lang['proj_launch_uploader_instruct_2'] = '从下拉栏选择负责你这个项目的 MC。';
+$lang['proj_launch_uploader_instruct_3'] = '你可以通过点击蓝色的"Start upload"（开始上载）按钮，一次上载所有选中的文件，或者用每个音频旁的按钮一个一个上传。';
+$lang['proj_launch_uploader_instruct_4'] = '上载完所有音频后，请复制每一个链接，并把它们粘贴到项目页面的新帖里。';
+$lang['proj_launch_uploader_notes'] = '注意：';
+$lang['proj_launch_uploader_notes_1'] = '如果你不在论坛里发布音频的链接，不会有人知道你已经上传过了它！';
+$lang['proj_launch_uploader_notes_2'] = '如果你在上传的是作测试用途的音频，请按你的 BC 提供的步骤操作，或按这份指南来：<a href="https://wiki.librivox.org/index.php/1-Minute_Test">https://wiki.librivox.org/index.php/1-Minute_Test</a>';
+$lang['proj_launch_uploader_notes_3'] = '如果你不小心选中了错误的文件，你可以在上传前点击 \'Cancel\'（取消）按钮，取消上传某些或全部文件。';
+$lang['proj_launch_uploader_notes_4'] = '如果你（用同一个文件名）上载了一个文件两次，第一个上传的文件会被自动重命名，从而避免被第二个文件盖写。';
+$lang['proj_launch_uploader_notes_5'] = '点击 \'Clear\'（清空）可以将文件移出列表，但不能把它从服务器上删除。若想删除文件，请联系管理人员。';
+$lang['proj_launch_uploader_help'] = '上载页面出错了？请重试几次；如果可能的话，换一个浏览器试试。如果问题还是解决不了，请在论坛向管理人员求助。';
+?>
