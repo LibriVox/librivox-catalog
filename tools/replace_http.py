71a9be1469fd215e3430e8268c38cd40f45079ef
Artom Lifshitz <notartom@gmail.com>
2021-11-14 14:21:37 -0500

    Update replace_http script to work with section MP3 URLs

--- tools/replace_http.py
+++ tools/replace_http.py
@@ -10,7 +10,7 @@ MYSQL_PASS = 'changeme'
 MYSQL_DB = 'librivox_catalog'
 
 
-def replace_http(column, replace=False):
+def replace_http(column, table, replace=False):
     HTTP_REGEX= re.compile(r'^http://')
 
     conn = pymysql.connect(user=MYSQL_USER, password=MYSQL_PASS,
@@ -19,21 +19,19 @@ def replace_http(column, replace=False):
                            cursorclass=pymysql.cursors.DictCursor)
 
     with conn.cursor() as cursor:
-        cursor.execute('SELECT id,%s FROM projects' % column)
-        # NOTE(artom) We could be smarter about this, but we only have ~16500
-        # projects.
-        projects = cursor.fetchall()
-        for project in projects:
-            url = project[column]
-            if url.startswith('http://'):
+        cursor.execute('SELECT id,%s FROM %s' % (column, table))
+        rows = cursor.fetchall()
+        for row in rows:
+            url = row[column]
+            if url and url.startswith('http://'):
                 new_url = HTTP_REGEX.sub('https://', url)
                 print('Will replace %s with %s... ' % (url, new_url),
                       end='', flush=True)
-                update = ('UPDATE projects '
-                          'SET ' + column + '=%(url)s '
+                update = ('UPDATE ' + table +
+                          ' SET ' + column + '=%(url)s '
                           'WHERE id=%(id)s')
                 if replace:
-                    cursor.execute(update, {'id': project['id'], 'url': new_url})
+                    cursor.execute(update, {'id': row['id'], 'url': new_url})
                     print('Done.')
                 else:
                     print('Dry run.')
@@ -46,8 +44,5 @@ if __name__ == '__main__':
                         default=False)
     args = parser.parse_args()
 
-    for column in [
-        'url_librivox', 'url_forum', 'coverart_pdf', 'coverart_jpg',
-        'coverart_thumbnail', 'url_text_source', 'url_project'
-    ]:
-        replace_http(column, replace=args.replace)
+    for column in ['mp3_64_url', 'mp3_128_url' ]:
+        replace_http(column, 'sections', replace=args.replace)
