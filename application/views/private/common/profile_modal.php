cbe362b9f02aeb2bcf14f65384792e5da57f45b5
Kevin Groeneveld <kgroeneveld@gmail.com>
2021-12-31 14:50:02 -0500

    Many updates to how fields are shown/enabled in the user profile screen.
    This started out as an attempt to implement the UI side of issue #83 which
    requested that only an MC (or admin) should be able to change the Display Name,
    Website or Max Project Count.
    
    At first it seemed like it would be easy to simply disable these fields based on
    your permissions. However the same form is used for adding users as for editing
    a user profile and these fields should not be disabled for the add case.
    
    While working on this I also came across some other bugs and inconsistencies in
    these screens. Some examples:
    1. Normally only admin can change the username. But if you first did Add New or
       Add Reader and then went to an edit profile screen then anyone could change
       the username (as the username field was made editable for the add and not
       disabled again later).
    2. If an error was displayed after pressing Save and then you closed the screen
       then the next time you went into the screen the now irrelevant error message
       was still there.
    
    Configuring the form for edit profile, add user or add reader were previously
    all handled by mostly separate code. I combined this all into one function named
    show_form which seemed to make things simpler overall. There is now only one
    spot where any field is shown/enabled such that you should always get consistent
    results (you cannot forget to re-enable a field for one case when it was
    disabled in another function).
    
    Fixing all of these things involved changing most of the lines in
    profile_modal.js. Since most of the file was changing anyway I went ahead and
    updated the white space on all lines to be consistent throughout the whole file.
    
    Fixes #83 (along with an earlier commit).

--- application/views/private/common/profile_modal.php
+++ application/views/private/common/profile_modal.php
@@ -12,27 +12,25 @@
             <input type="hidden" id="action" name="action" value="update">
             <input type="hidden" id="user_id" name="user_id">
 
-            <?php $disabled = ($this->ion_auth->is_admin())? '': ' disabled '; ?>
-
              <div class="control-group">
                  <div class="controls ">
                         <label for="username" ><span class="span2"><span class="red" style="margin-right:4px;">*</span>User Name</span>
-                        <input type="text" class="no-margin span3" name="username" id="username" <?= $disabled ?>>                        
+                        <input type="text" class="no-margin span3" name="username" id="username">
                         </label>
                  </div>
              </div>
              
              <div class="control-group">
                  <div class="controls ">
-                        <label for="display_name" ><span class="span2"><span class="red" style="margin-right:4px;">*</span> Display Name</span>
-                        <input type="text" class="no-margin span3" name="display_name" id="display_name">                        
+                        <label for="display_name" ><span class="span2"><span class="red" style="margin-right:4px;">*</span>Display Name</span>
+                        <input type="text" class="no-margin span3" name="display_name" id="display_name">
                         </label>
                  </div>
              </div>
 
              <div class="control-group">
                  <div class="controls ">
-                        <label for="email"><span class="span2"><span class="red" style="margin-right:4px;">*</span> Email</span>
+                        <label for="email"><span class="span2"><span class="red" style="margin-right:4px;">*</span>Email</span>
                         <input type="text" class="no-margin span3" name="email" id="email">                        
                         </label>
                  </div>
@@ -40,16 +38,16 @@
 
              <div class="control-group">
                  <div class="controls ">
-                        <label for="website"><span class="span2" ><span class="red" style="margin-right:8px;">&nbsp;</span>Website</span>
-                        <input type="text" class="no-margin span3" name="website" id="website">                        
+                        <label for="website"><span class="span2" ><span class="red" style="margin-right:4px;">&nbsp;</span>Website</span>
+                        <input type="text" class="no-margin span3" name="website" id="website">
                         </label>
                  </div>
              </div>
 
              <div class="control-group">
                  <div class="controls ">
-                        <label for="max_projects"><span class="span2"><span class="red" style="margin-right:4px;">*</span> Max Project Count</span>
-                        <input type="text" class="no-margin span3" name="max_projects" id="max_projects">                        
+                        <label for="max_projects"><span class="span2"><span class="red" style="margin-right:4px;">*</span>Max Project Count</span>
+                        <input type="text" class="no-margin span3" name="max_projects" id="max_projects">
                         </label>
                  </div>
              </div>
@@ -58,7 +56,7 @@
 
              <div class="control-group">
                  <div class="controls ">
-                        <label for="password"><span class="span2" ><span class="red" style="margin-right:8px;">&nbsp;</span>Password</span>
+                        <label for="password"><span class="span2" ><span class="red" style="margin-right:4px;">&nbsp;</span>Password</span>
                         <input type="password" class="no-margin span3" name="password" id="password">                        
                         </label>
                  </div>
@@ -66,7 +64,7 @@
 
              <div class="control-group">
                  <div class="controls ">
-                        <label for="confirm_password"><span class="span2" ><span class="red" style="margin-right:8px;">&nbsp;</span>Confirm Password</span>
+                        <label for="confirm_password"><span class="span2" ><span class="red" style="margin-right:4px;">&nbsp;</span>Confirm Password</span>
                         <input type="password" class="no-margin span3" name="confirm_password" id="confirm_password">                        
                         </label>
                  </div>
@@ -74,13 +72,13 @@
 
              <div id="groups_block">
 
-                <p id="groups_label" style="margin-left: 40px;"><strong>Add Roles:</strong></p>
+                <p id="groups_label" style="margin-left: 40px;"><strong>Roles:</strong></p>
 
                 <?php foreach ($roles as $role): ?>
                  <div class="control-group">
                      <div class="controls ">
                             <label for="groups"><span class="span2" ><span class="red" style="margin-right:8px;">&nbsp;</span><?= $role['label'];?></span>
-                            <input type="checkbox" class="group_box" name="groups[]" id="groups_<?= $role['value'];?>" value="<?= $role['value'];?>" <?= ($role['checked']) ? 'checked':''; ?>>                        
+                            <input type="checkbox" class="group_box" name="groups[]" id="groups_<?= $role['value'];?>" value="<?= $role['value'];?>" <?= $role['checked'] ? 'checked' : ''; ?> <?= $role['disabled'] ? 'disabled' : '' ?>>
                             </label>
                      </div>
                  </div> 
