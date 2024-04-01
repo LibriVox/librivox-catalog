32d03d9b3686a325dd33f17b891a308ac2114efc
Artom Lifshitz <alifshit@redhat.com>
2024-04-01 11:13:16 -0400

    Simplify DB migration controller
    current() actually updates the DB, there's no need to call current()
    to get the version, and then to call version(). Just call current()
    directly.

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -6,12 +6,10 @@ class Migrate extends CI_Controller
     public function migrate()
     {
         $this->load->library('migration');
-        $target_version = $this->migration->current();
-        if ($target_version)
+
+        if ($this->migration->current() === FALSE)
         {
-            $this->migration->version($target_version);
-        } else {
             show_error($this->migration->error_string());
-        } 
+        }
     }
 }
