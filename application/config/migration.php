22e2a9fd3bea012f7e24ec13d5dbccc10f8eb005
parallels <peterjdann@gmail.com>
2024-04-02 14:14:13 +1100

    Added (coded) migration instructions for creation of 'instances' field in 'keywords' table

--- application/config/migration.php
+++ application/config/migration.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['migration_enabled'] = TRUE;
-$config['migration_type'] = 'sequential';
-$config['migration_version'] = 0;
+$config['migration_type'] = 'timestamp';
+$config['migration_version'] = 202404020149;
 $config['migration_path'] = APPPATH . 'migrations/';
