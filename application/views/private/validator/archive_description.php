0e65f5267585354c8591a2d7e12c4c88b00b3e26
redrun <redrun45@readyour.email>
2024-04-10 03:30:24 +0000

    Fix Archive 'Read by' list not matching catalog Fixes #201.  Per MC discussion, we want Archive pages to match the existing 'Read by' label on our catalog pages: * All types of group projects show 'LibriVox Volunteers', regardless of     reader count. * Solo projects show the first reader result returned from the database,     or 'Solo' if there are none.
    Along with this change, I've refactored `get_project_readers` and
    `create_project_reader_list` into `get_solo_reader`:
    * `get_project_readers` was called only from catalog pages, but only for
        solo projects, and only to get the first reader result.
    * `create_project_reader_list` is no longer needed.  It was called only
        from Iarchive_upload, which should also only need to call it for solo
        projects.
    
    If you think it would be better to have the group vs solo check be in
    the model rather than the controllers, we could do that at the cost of a
    second (but quick) database query.  Open to suggestions!

--- application/views/private/validator/archive_description.php
+++ application/views/private/validator/archive_description.php
@@ -1,5 +1,5 @@
 <a href="https://librivox.org/">LibriVox</a> recording of <?= $title ?> by <?= $authors ?>. <?= $translators ?><br />
-Read in <?= $language ?> by <?= $readers ?><br /><br />
+Read in <?= $language ?> by <?= $reader_name ?><br /><br />
 <?= $description ?><br /><br />
 For further information, including links to online text, reader information, RSS feeds, CD cover or other formats (if available), please go to the <a href="<?= $catalog_url ?>">LibriVox catalog page</a> for this recording.<br /><br />
 For more free audio books or to become a volunteer reader, visit <a href="https://librivox.org/">librivox.org</a>.<br /><br />
