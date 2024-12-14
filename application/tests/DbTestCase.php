200ac9f68ac6be4067a5be2e2dada8f9d64ef2c1
Artom Lifshitz <alifshit@redhat.com>
2024-12-14 17:23:47 -0500

    Add DB seed and tests for coverart in API
    Following up on PR#245, add a DataBase Seeder that uses our scrubbed
    database dump
    (https://github.com/LibriVox/librivox-ansible/blob/master/roles/db_import/files/librivox_catalog_scrubbed.sql.bz2), and start using it to test coverart in the API.

--- application/tests/DbTestCase.php
+++ application/tests/DbTestCase.php
@@ -2,4 +2,12 @@
 
 class DbTestCase extends CIPHPUnitTestDbTestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $CI =& get_instance();
+		$CI->load->library('Seeder');
+		$CI->seeder->call('DatabaseSeeder');
+    }
 }
