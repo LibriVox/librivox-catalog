44ef29f225f736431d4ecd2e76c327951c8b52c8
Artom Lifshitz <alifshit@redhat.com>
2025-05-13 12:39:52 -0400

    FUP to PR#231 - keywords search dropdown for advanced search
    * Remove migration and rely on cron
    * Remove APCU as it's a key-value cache, and doesn't make much sense
      to use for a single JSON blob

--- application/controllers/cron/Keywords_cache_update.php
+++ application/controllers/cron/Keywords_cache_update.php
@@ -21,29 +21,7 @@ class Keywords_cache_update extends CI_Controller
 		$query = $this->db->query($sql);
 		$json_encoded_keywords_cache = json_encode($query->result_array());
 		
-		// Use following two lines for file-based cache.
-		// If using file-based cache, make sure that autocomplete() function in 
-		// application/models/Keyword_model.php is also using file-based caching
-
 		$this->load->helper('file');
 		write_file(CACHE_DIR . KEYWORDS_CACHE_FILENAME, $json_encoded_keywords_cache);
-
-/**		
-		// Use following for apcu cache.
-		// If using apcu cache, make sure that autocomplete() function in 
-		// application/models/Keyword_model.php is also using apcu caching
-	
-		if (apcu_exists(KEYWORDS_CACHE_KEY_APCU))
-		{			
-			// overwrite existing data for this key
-			apcu_store(KEYWORDS_CACHE_KEY_APCU, $json_encoded_keywords_cache);
-		}
-		else
-		{
-			// must use apcu_add if key not already present in cache
-			apcu_add(KEYWORDS_CACHE_KEY_APCU, $json_encoded_keywords_cache);
-		}
-**/
-
 	}
 }
--- application/models/Keyword_model.php
+++ application/models/Keyword_model.php
@@ -10,22 +10,7 @@ class Keyword_model extends MY_Model {
 
     }
     
- /**   
-    	public function get_all_keywords_used_in_projects()
-    	{
-    		error_log("In get_all_ keywords_used_in_project");
-    		$sql = 'SELECT DISTINCT k.value
-		FROM keywords k
-		JOIN project_keywords pk
-		ON k.id = pk.keyword_id';
-		
-		$query = $this->db->query($sql);
-		return $query->result_array();
-    	}
-    	
-  **/  	
-    	
-    	public function autocomplete($term)
+    public function autocomplete($term)
 	{
 		// To minimise the number of database calls in our system if multiple
 		// users are entering values in the Advanced Search Keywords field at the
@@ -36,17 +21,8 @@ class Keyword_model extends MY_Model {
 		$json_encoded_keywords_cache_as_read_from_file = '';
 		$cached_result_array = array();
 		
-		// Use following two lines for file-based cache.
-		// If using file-based cache, make sure that /application/controllers/cron/Keywords_cache_update
-		// is writing a file-based cache
 		$this->load->helper('file');
 		$json_encoded_keywords_cache = read_file(CACHE_DIR . KEYWORDS_CACHE_FILENAME);
-		
-		// Use following line for apcu cache.
-		// If using apcu cache, make sure that /application/controllers/cron/Keywords_cache_update
-		// is writing to apcu cache
-		
-		// $json_encoded_keywords_cache = apcu_fetch(KEYWORDS_CACHE_KEY_APCU);
 		
 		$associative = true;
 		$keywords_cache_array = json_decode($json_encoded_keywords_cache, $associative);
@@ -102,50 +78,6 @@ class Keyword_model extends MY_Model {
 					
 		return $cached_result_array;
 	}  // end of function
-		
-/**
-		// The following code achieves a result similar to the cache-based system
-		// above, but without case-insensitive matching. The argument for favouring
-		// a cache-based system over the one below is that the one below makes heavier
-		// demands on our database and shows poorer performance under load testing.
-		
-		// Leaving the code below here for the present to facilitate easy comparison
-		// of caching versus non-caching approaches
-		
-		// Escaping -- https://www.codeigniter.com/userguide3/database/queries.html#escaping-queries
-		$escaped_term = $this->db->escape_like_str($term);
-		
-		// For extra safety, parameterise the query as well
-		
-		$params = [];
-		array_push($params, $escaped_term . "%");
-		array_push($params, $escaped_term);
-		array_push($params, "%" . $escaped_term . "%");
-		array_push($params, $escaped_term . "%");
-
-		$sql = 'SELECT DISTINCT k.value, "A" AS priority
-		FROM keywords k
-		JOIN project_keywords pk
-		ON k.id = pk.keyword_id
-		WHERE k.value LIKE ?  
-		UNION 
-		SELECT "=== some other keywords containing \"?\" ===" AS value, "B" AS priority
-		UNION
-		SELECT DISTINCT k.value, "C" AS priority
-		FROM keywords k
-		JOIN project_keywords pk
-		ON k.id = pk.keyword_id
-		WHERE k.value LIKE ?  
-			AND k.value NOT LIKE ?  		
-		ORDER BY priority ASC, value ASC
-		LIMIT 200';
-		
-		$query = $this->db->query($sql, $params);
-		return $query->result_array();
-	
-
-	}	
-**/
 
 	//return comma delimited list of keywords from project
 	public function create_keyword_list($project_id)
