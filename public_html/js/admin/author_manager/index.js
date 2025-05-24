0bee258d24cb6172cab58ae08f28fc94d2a891ec
redrun <redrun45@readyour.email>
2025-05-23 22:59:00 -0500

    Author Manager: load fewer records by default This commit adds some navigation options, rather than loading all author records at once (at least by default).  It does NOT add real pagination, which is evidently something that our dataTables library supports[1].
    From the user side:
    1. The client-side control (and JS code) for toggling between
     unconfirmed authors and *all* authors has been replaced with a pair of
     navigation buttons.  'unconfirmed' is the default, and is _faster_.
    2. dataTables will now hide its client-side pagination and search
     controls, if there are 10 or fewer authors in the navigation selection.
    3. Authors can be navigated individually by name, using the new,
     auto-completing search box (based on the one in Section Compiler).
    
    [1]: https://datatables.net/manual/server-side
    ...though, at version 1.9.2, we're officially in legacy-land.

--- public_html/js/admin/author_manager/index.js
+++ public_html/js/admin/author_manager/index.js
@@ -24,8 +24,10 @@ $(document).ready(function() {
 
     bind_edit();
 
+    var table_size = $('#authors_table').find('tr').length;
+
     $.extend( $.fn.dataTable.defaults, {
-        "bFilter": true,
+        "bFilter": table_size > 10 + 1, // Header row counts!
         "bPaginate" : true,
         "bInfo" : true,
         "sPaginationType": "full_numbers",
@@ -39,29 +41,6 @@ $(document).ready(function() {
     var oTable = $('#authors_table').dataTable();
 
     oTable.fnSort([[1, 'asc']]);
-    
-
-    //default  -only unconfirmed
-    // need to make this check which radio button is checked
-    var checked_radio = $('input[class=status_group]:checked').val();    
-    apply_filter(checked_radio);  
-
-    $('.status_group').live('click', function(){   
-        apply_filter( $(this).val());
-    }) ; 
-
-    function apply_filter(filter_value)
-    {
-        if (filter_value == '1') 
-        {
-            oTable.fnFilter( 0, 0 );
-        }
-        else
-        {
-            oTable.fnFilter('', 0);
-            //oTable.fnFilter( '' );
-        }   
-    }
 
     function bind_edit()
     {
@@ -126,11 +105,6 @@ $(document).ready(function() {
                 var row_id = oTable.fnGetPosition(row);
 
                 update_author_status(value, row_id);
-
-                //the one from above was static
-                var sub_checked_radio = $('input[class=status_group]:checked').val();
-                apply_filter(sub_checked_radio);
-     
               },
         });   
 
@@ -337,3 +311,28 @@ $(document).ready(function() {
     });
 
 });
+
+
+// Autocomplete the "Load author by name" search
+
+function autocomplete_assign_vars(item)
+{
+    var name_field;
+    if (item.username == undefined) {
+        name_field = author_string(item);
+    } else {
+        name_field = item.username;
+    }
+
+    return {
+        label: name_field,
+        value: name_field,
+        source_id: item.id,
+        source_name: name_field,
+    }
+}
+
+function autocomplete_assign_elements(search_area, ui)
+{
+    window.location.href = CI_ROOT + 'admin/author_manager/' + ui.item.source_id
+}
